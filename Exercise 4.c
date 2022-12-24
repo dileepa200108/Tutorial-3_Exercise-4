f14a21b2b7189b324bb414cdb75d59ad792e3627
dileep <>
2022-12-24 16:15:21 +0530

    Added exercise 4

---
+++ Exercise 4.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int main(){
+int m;
+printf("Enter IP Module Marks :");
+scanf("%d",&m);
+if (100>=m || m>=80){
+	printf("A");
+}
+else if(79>=m || m>=70 ){
+	printf("B");
+	
+}
+else if(m<=69 || m>=45){
+	printf("C");
+}
+else if(m<45){
+	printf("F");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
